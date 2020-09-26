98e31987611fc5132ffc376c7fb5e0cf8e0f2d9b
Hasstrup Ezekiel <hasstrup.ezekiel@itsmycargo.com>
2020-09-26 05:06:45 +0100

    [feat] - annotation and change to postgres

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -2,3 +2,15 @@ class Expense < ApplicationRecord
   validates :amount, :date, :description, presence: true
   validates :amount, numericality: { greater_than: 0, only_integer: true }
 end
+
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :bigint           not null, primary key
+#  amount      :integer
+#  date        :date
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
